e7ffa0fa9b476dbc956eedb490cabbabb76b5d1e
Gibran Essa <gibran.essa@gmail.com>
2017-10-15 00:53:51 -0400

    old

--- src/helpers/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export const config = {
-    apiKey: "AIzaSyC4hI9EJEgtlyuTE_S-s-LHnZ-AOR8MLmo",
-    authDomain: "bike-track-238aa.firebaseapp.com",
-    databaseURL: "https://bike-track-238aa.firebaseio.com",
-    projectId: "bike-track-238aa",
-    storageBucket: "bike-track-238aa.appspot.com",
-    messagingSenderId: "383183824291"
-  };
